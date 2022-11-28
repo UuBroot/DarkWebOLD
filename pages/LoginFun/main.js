35df04579b296261067d59fe25472511df412a6d
Timon Rosenbichler <timon.rosenbichler@gmx.at>
2022-11-28 18:44:07 +0100

    made loginpagei n folder

---
+++ pages/LoginFun/main.js
@@ -0,0 +1,16 @@
+function getRandomWebsites() {
+    let random = Math.floor(Math.random()*2+0);
+    console.log(random);
+    switch (random) {
+        case 0:
+        window.open('https://uubroot.github.io/DarkWeb/pages/FavouriteColor/index.html', '_self');
+            break;
+        case 1:
+            window.open('https://uubroot.github.io/DarkWeb/pages/soggyCat/index.html', '_self');
+            break;
+    }
+}
+
+function openFirstPage() {
+    window.open('pages/Login/index.html', '_self');
+}
