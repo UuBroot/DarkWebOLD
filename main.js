4c126925ab0517e71101933ea9ee977e34d304d8
fishy52 <derfisch04@gmail.com>
2022-11-28 20:11:53 +0100

    Super Idol

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function getRandomWebsites() {
-    let random = Math.floor(Math.random()*6+0);
+    let random = Math.floor(Math.random() * 7 + 0);
     console.log(random);
     switch (random) {
         case 0:
-        window.open('pages/FavouriteColor/index.html', '_blank');
+            window.open('pages/FavouriteColor/index.html', '_blank');
             break;
         case 1:
             window.open('pages/SoggyCat/index.html', '_blank');
@@ -16,9 +16,12 @@ function getRandomWebsites() {
             break;
         case 4:
             window.open('pages/LoginFun/index.html', '_blank');
-            break;   
+            break;
         case 5:
             window.open('pages/TheBestMovies/index.html', '_blank');
-            break;     
+            break;
+        case 6:
+            window.open('pages/boned/index.html', '_blank');
+            break;
     }
 }
