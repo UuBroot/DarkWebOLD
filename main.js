1b609fcb6acdab71403788ce70550f66f5e5e642
Timon Rosenbichler <timon.rosenbichler@gmx.at>
2022-11-28 18:51:13 +0100

    GABRIEL LERN SCHREIBEN BITTE

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ function getRandomWebsites() {
     console.log(random);
     switch (random) {
         case 0:
-        window.open('https://uubroot.github.io/DarkWeb/pages/FavouriteColor/index.html', '_self');
+        window.open('https://uubroot.github.io/DarkWeb/pages/FavouriteColor/index.html', '_blank');
             break;
         case 1:
-            window.open('https://uubroot.github.io/DarkWeb/pages/soggyCat/index.html', '_self');
+            window.open('https://uubroot.github.io/DarkWeb/pages/SoggyCat/index.html', '_blank');
+            break;
+        case 2:
+            window.open('https://uubroot.github.io/DarkWeb/pages/BlackMarket/index.html', '_blank');
+            break;
+        case 3:
+            window.open('https://uubroot.github.io/DarkWeb/pages/chinese_button/index.html', '_blank');
             break;
     }
-}
-
-function openFirstPage() {
-    window.open('pages/Login/index.html', '_self');
 }
