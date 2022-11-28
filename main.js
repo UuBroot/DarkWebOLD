95603690401fb9b91d93043889c6757242808e62
Fishy <118261802+fishy52@users.noreply.github.com>
2022-11-28 13:34:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function getRandomWebsites() {
         window.open('https://uubroot.github.io/DarkWeb/pages/FavouriteColor/index.html', _self);
             break;
         case 1:
-            window.open('https://uubroot.github.io/DarkWeb/pages/soggyCat/index.html' _self);
+            window.open('https://uubroot.github.io/DarkWeb/pages/soggyCat/index.html', _self);
             break;
     }
 }
