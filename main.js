2f4f516700b163e3e7a0dfc10ab1884d13dd69d9
Timon Rosenbichler <timon.rosenbichler@gmx.at>
2022-11-28 12:43:40 +0100

    tried addeing first page

---
+++ main.js
@@ -0,0 +1,11 @@
+function getRandomWebsites() {
+    let random = Math.floor(Math.random()*0+0);
+    console.log(random);
+    switch (random) {
+        case 0:
+        window.open('https://uubroot.github.io/DarkWeb/pages/soggyCat/index.html', '_blank');
+            break;
+        case 1:
+            break;
+    }
+}
