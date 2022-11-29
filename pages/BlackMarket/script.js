41cd46528ebb5c8c58f5732a33640fb36bfff2ce
fishy52 <derfisch04@gmail.com>
2022-11-29 11:50:04 +0100

    You can now buy Items at the black market

--- pages/BlackMarket/script.js
+++ pages/BlackMarket/script.js
@@ -1,3 +1,18 @@
-function openItem() {
-    window.open('itemPage.html', '_self');
+let itemNames = ['Nerf', 'Snickels', 'Switch'];
+let itemImages = ['cursed_Nerf.webp', 'snickles2.png', 'cursed_Switch2.jpg'];
+
+function openItem(itemID) {
+    console.log("lmao");
+    document.getElementById("body").innerHTML = `
+    <h1 id="itemName">` + itemNames[itemID] + `</h1>
+    <center><img width="20%" src="img/` + itemImages[itemID] + `"></center>
+    <br>
+    <center><button onclick="buy(` + itemID + `)">Buy now!</button></center>
+    `;
+
+    document.getElementById("itemName").innerHTML = itemNames[itemID];
+}
+
+function buy(itemID) {
+    window.open('donate.html', '_blank')
 }
