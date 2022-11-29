c3e1e1f743fac97a4401ef1ed8826925df25061c
fishy52 <derfisch04@gmail.com>
2022-11-29 22:51:15 +0100

    You can now bribe us

--- pages/SoggyCat/main.js
+++ pages/SoggyCat/main.js
@@ -169,6 +169,6 @@ function showPussy() {
 		setTimeout(function () {
 			popupWindow = window.open('popUp.html', 'popUpWindow', 
 			'height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
-		}, 3000);
+		}, 30000);
 	}
 }
