c6925ee91ee34e185b9f58a3a9b12c4a439d2e83
fishy52 <derfisch04@gmail.com>
2022-11-29 08:52:53 +0100

    Reworked the random code in main.js

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
 function getRandomWebsites() {
-    let random = Math.floor(Math.random() * 7 + 0);
+    let urls = [
+        'pages/SoggyCat/index.html', 'pages/BlackMarket/index.html', 'pages/chinese_button/index.html', 'pages/LoginFun/index.html',
+        'pages/TheBestMovies/index.html', 'pages/boned/index.html'
+    ];
+
+
+    let random = Math.floor(Math.random() * urls.length);
     console.log(random);
-    switch (random) {
-        case 0:
-            window.open('pages/FavouriteColor/index.html', '_blank');
-            break;
-        case 1:
-            window.open('pages/SoggyCat/index.html', '_blank');
-            break;
-        case 2:
-            window.open('pages/BlackMarket/index.html', '_blank');
-            break;
-        case 3:
-            window.open('pages/chinese_button/index.html', '_blank');
-            break;
-        case 4:
-            window.open('pages/LoginFun/index.html', '_blank');
-            break;
-        case 5:
-            window.open('pages/TheBestMovies/index.html', '_blank');
-            break;
-        case 6:
-            window.open('pages/boned/index.html', '_blank');
-            break;
-    }
+    window.open(urls[random], '_blank');
 }
