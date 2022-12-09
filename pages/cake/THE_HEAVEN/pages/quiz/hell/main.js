4d4d248cb8496405af0c96da439455c037b8edf5
Timon Rosenbichler <timon.rosenbichler@gmx.at>
2022-12-09 09:31:23 +0100

    hell

---
+++ pages/cake/THE_HEAVEN/pages/quiz/hell/main.js
@@ -0,0 +1,9 @@
+function makePage() {
+    document.getElementById("body").innerHTML = `
+    <div style="display: flex;">
+    <img src="" alt="">
+    <h1>Hell</h1>
+</div>
+    `;
+    document.getElementById("body").style.backgroundImage ="background.webp";
+}
